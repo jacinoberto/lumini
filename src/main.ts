77a7f0b42e4466fd6e39b2594bb0c82b769e8557
Jacimara Noberto <estudo.jacimaranoberto@gmail.com>
2025-10-27 22:36:16 -0300

    corrigindo deploy

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import './assets/styles/main.css'; // Certifique-se que o caminho está correto
 import router from '@/router/index'; // Corrigido para remover '.ts'
 import VueFeather from 'vue-feather';
 import { mask } from 'vue-the-mask';
-import api from './services/api'; // 1. Importe a instância do Axios
+import axios from 'axios';
 
 const app = createApp(App);
 
@@ -18,7 +18,7 @@ app.use(router);
 // --- LÓGICA CSRF ---
 // 2. Chama o endpoint para obter o cookie CSRF
 //    CONFIRME ESTE ENDPOINT com o backend (Orion). '/sanctum/csrf-cookie' é o padrão do Laravel Sanctum.
-api.get('/sanctum/csrf-cookie').then(() => {
+axios.get('https://api-lumini.onrender.com/sanctum/csrf-cookie').then(() => {
     // 3. Monte o aplicativo SOMENTE APÓS obter o cookie com sucesso
     app.mount('#app');
 }).catch(error => {

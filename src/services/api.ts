ea19c1441409ad38657adda323611e4f9a047ee4
Jacimara Noberto <estudo.jacimaranoberto@gmail.com>
2025-10-27 22:01:14 -0300

    adicionando csrf

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,8 @@ const api = axios.create({
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
-        'Accept': 'application/json'
+        'Accept': 'application/json',
+        withCredentials: true,
     }
 });
 

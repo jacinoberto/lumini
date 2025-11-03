edf69a769870158e0645cd2eb950025de5f149df
Jacimara Noberto <estudo.jacimaranoberto@gmail.com>
2025-11-03 16:51:28 -0300

    build

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import devtools from 'vite-plugin-vue-devtools'
 
-// Use a função de configuração para aceder ao 'command'
 export default defineConfig(({ command }) => ({
     plugins: [
         vue(),
-        // Adiciona o devtools apenas se o comando NÃO for 'build'
-        command !== 'build' && devtools(),
+        // 'command' é 'serve' (dev) ou 'build' (prod).
+        // Esta é a forma mais segura de o desativar no build.
+        command === 'serve' && devtools(),
     ],
 }))

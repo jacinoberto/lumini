56ad3afa9ab725b202ecdd098e056f1f77377d31
Jacimara Noberto <estudo.jacimaranoberto@gmail.com>
2025-11-03 17:54:52 -0300

    build 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import devtools from 'vite-plugin-vue-devtools'
-import path from 'path'
 
-export default defineConfig(({ command }) => ({
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-        },
-    },
+// https://vite.dev/config/
+export default defineConfig({
     plugins: [
         vue(),
-        command === 'serve' && devtools(),
-    ].filter(Boolean),
-}))
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            'vue': 'vue/dist/vue.esm-bundler.js', // <-- ESSENCIAL
+        },
+    },
+})
+t
